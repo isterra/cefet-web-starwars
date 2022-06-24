e2b33473b16d6e48b4f39ced2b968536c85cb416
Israel Amaral Linhares Terra <55004305+isterra@users.noreply.github.com>
2022-06-24 19:55:27 -0300

    ex1

--- starwars.js
+++ starwars.js
@@ -3,5 +3,15 @@
 // para fazer uma requisição assíncrona e:
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
-
+import {play} from "./music.js"
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+let audioConfig={
+    audioUrl: "./audio/tema-sw.mp3",
+    coverImageUrl: "./imgs/logo.svg",
+    title: "Intro",
+    artist: "John Williams",
+  };
+  
+play(audioConfig,document.body)
+
